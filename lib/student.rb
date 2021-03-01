824a789ca319209a36ca58b8c9e95114bb5b12a7
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-01 20:05:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,11 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :name, :grade
+  attr_reader :id
   
   def initialize(id=nil, name, grade)
     @id = id
