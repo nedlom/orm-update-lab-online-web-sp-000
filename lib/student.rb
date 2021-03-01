1ff31adb74fb6d5315f156aae70863d92c5cbaa4
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-01 20:20:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,5 +38,11 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
 
 end
